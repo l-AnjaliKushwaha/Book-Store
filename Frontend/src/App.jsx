a7b7f630f4aa80adb9838fe5337e52ee58e282da
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-07-10 09:11:22 +0530

    "Added react-icons dependency and implemented Navbar component with react-router-dom and react-icons"

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from "./components/Navbar.jsx"
 
 function App() {
 
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+
+// react icons
+import {FaBlog } from "react-icons/fa6";
 
 function Navbar() {
+  const [isMenuOpen, seIsMenuOpen] = useState(false);
+  const [isSticky, setSticky] = useState(false);
+
+  //toggle menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY > 100){
+        setSticky(true);
+      }
+      else{
+        setSticky(false);
+      }
+    }
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+       window.addEventListener("scroll", handleScroll);
+    }
+  }, [])
+
+  // navItems here
+  const navItems = [
+    { link: "Home", path: "/" },
+    { link: "About", path: "/about" },
+    { link: "Shop", path: "/shop" },
+    { link: "Sell Your Book", path: "/admin/dashboard" },
+    { link: "Blog", path: "/Blog" },
+  ];
+
   return (
-    <div>
-      Navbar
-    </div>
+    <header>
+      <nav>
+        <div>
+          {/* logo */}
+          <Link to="/"><FaBlog/>Books</Link>
+        </div>
+      </nav>
+    </header>
   )
 }
 
